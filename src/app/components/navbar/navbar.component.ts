c7edf135c452aa041f2925dcd01c6913aec468b2
Diego Juan Isaquiel <diego.mizael@facens.br>
2023-02-28 16:10:13 -0300

    feat(components):Created navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LanguagesEnum } from 'src/app/models/enum/languages.enum';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  //#region Constructor
 
-  ngOnInit(): void {
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+  ) {
+    this.selectedLanguage = this.route.snapshot.paramMap.get('language');
+
+    if (this.selectedLanguage != this.languagesEnum.ENGLISH && this.selectedLanguage != this.languagesEnum.PORTUGUESE)
+      this.router.navigate(['/pages/main']);
+
   }
+
+  //#endregion
+
+  //#region Public Properties
+
+  public languagesEnum: typeof LanguagesEnum = LanguagesEnum
+
+  public selectedLanguage: string | null = '';
+
+  public isMouseOver: boolean = false;
+
+  //#endregion
 
 }
